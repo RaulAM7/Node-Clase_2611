8fc0bea150ce2755764a9267d9cb78cdcd2cd9f3
RaulAM <raulartilesm@gmail.com>
2024-11-26 05:18:40 +0000

    objeto global learned

---
+++ index.js
@@ -0,0 +1,15 @@
+/* Es una variable global en toda nuestra aplicación
+
+    En el navegador apunta al objeto window
+
+    En Node.js apunta a una variable global
+
+    En Node es la forma correcta de referirse a la variable global que apunta al objeto global
+
+*/
+
+console.log(globalThis)
+console.log()
+console.log(global)
+console.log()
+console.log(window)
